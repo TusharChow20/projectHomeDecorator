8f93384b2d530de7f8d8b23ba774801a31f1ff04
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2025-10-14 01:44:33 +0600

    error animation from react lotti

--- src/Pges/Error.jsx
+++ src/Pges/Error.jsx
@@ -1,18 +1,34 @@
 import React from "react";
 import Navbar from "../Component/Navbar";
 import Footer from "../Component/Footer";
+import Lottie from "lottie-react";
+import errorAnimation from "../Assets/file1.json";
+import { Link } from "react-router";
 
 const Error = () => {
   return (
-    <div>
-      <Navbar></Navbar>
-      <main className="min-h-[calc(100vh-258px)]">
-        <h1>This is error</h1>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-1 flex flex-col items-center justify-center p-4">
+        <div className="w-full max-w-md">
+          <Lottie animationData={errorAnimation} loop={true} />
+        </div>
+        <h1 className="text-3xl font-bold text-center mt-6 text-gray-700">
+          Oops! Page Not Found
+        </h1>
+        <p className="text-gray-500 text-center mt-2 mb-6">
+          The page you are looking for doesn’t exist or has been moved.
+        </p>
+        <Link
+          to="/"
+          className="bg-indigo-500 text-white px-5 py-2 rounded-xl hover:bg-indigo-600 transition"
+        >
+          Go Back Home
+        </Link>
       </main>
-      <Footer></Footer>
+      <Footer />
     </div>
   );
 };
 
 export default Error;
-<h1>This is error</h1>;
